550a30ba6dd543ad6395b1e757273a20642fadc0
Dmitriyrus99 <Dmitriyrus99@gmail.com>
2025-06-20 18:46:19 +0300

    Fix lint issues and update typing

--- ferum_customs/hooks.py
+++ ferum_customs/hooks.py
@@ -1,239 +1,39 @@
+# Ferum Customs – hooks
+from .custom_hooks import DOC_EVENTS
+
 app_name = "ferum_customs"
-app_title = "ferum_customs"
-app_publisher = "ferum_customs"
-app_description = "ferum_customs"
-app_email = "client@ferumrus.ru"
-app_license = "mit"
+app_title = "Ferum Customs"
+app_publisher = "Ferum LLC"
+app_description = "Specific custom functionality for ERPNext"
+app_email = "support@ferum.ru"
+app_license = "MIT"
 
-# Apps
-# ------------------
+doc_events = DOC_EVENTS
+get_notification_config = "ferum_customs.notifications.notifications.get_notification_config"
 
-# required_apps = []
+# ── порядок фикстур: сначала DocType, затем поля/права, затем данные ──
+fixtures = [
+	"service_request.json",
+	"service_object.json",
+	"service_project.json",
+	"custom_fields.json",
+	"custom_docperm.json",
+	"audit_log.json",
+	"workflow_service_request.json",
+	"portal_menu_item.json",
+	"role.json",
+	"notification.json",
+	"users.json",
+	"customer.json",
+]
 
-# Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "ferum_customs",
-# 		"logo": "/assets/ferum_customs/logo.png",
-# 		"title": "ferum_customs",
-# 		"route": "/ferum_customs",
-# 		"has_permission": "ferum_customs.api.permission.has_app_permission"
-# 	}
-# ]
 
-# Includes in <head>
-# ------------------
+def scheduler_events() -> dict:
+	"""Return scheduler events configuration for Frappe."""
+	return {}
 
-# include js, css files in header of desk.html
-# app_include_css = "/assets/ferum_customs/css/ferum_customs.css"
-# app_include_js = "/assets/ferum_customs/js/ferum_customs.js"
 
-# include js, css files in header of web template
-# web_include_css = "/assets/ferum_customs/css/ferum_customs.css"
-# web_include_js = "/assets/ferum_customs/js/ferum_customs.js"
-
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "ferum_customs/public/scss/website"
-
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
-
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
-
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-
-# Svg Icons
-# ------------------
-# include app icons in desk
-# app_include_icons = "ferum_customs/public/icons.svg"
-
-# Home Pages
-# ----------
-
-# application home page (will override Website Settings)
-# home_page = "login"
-
-# website user home page (by Role)
-# role_home_page = {
-# 	"Role": "home_page"
-# }
-
-# Generators
-# ----------
-
-# automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
-
-# automatically load and sync documents of this doctype from downstream apps
-# importable_doctypes = [doctype_1]
-
-# Jinja
-# ----------
-
-# add methods and filters to jinja environment
-# jinja = {
-# 	"methods": "ferum_customs.utils.jinja_methods",
-# 	"filters": "ferum_customs.utils.jinja_filters"
-# }
-
-# Installation
-# ------------
-
-# before_install = "ferum_customs.install.before_install"
-# after_install = "ferum_customs.install.after_install"
-
-# Uninstallation
-# ------------
-
-# before_uninstall = "ferum_customs.uninstall.before_uninstall"
-# after_uninstall = "ferum_customs.uninstall.after_uninstall"
-
-# Integration Setup
-# ------------------
-# To set up dependencies/integrations with other apps
-# Name of the app being installed is passed as an argument
-
-# before_app_install = "ferum_customs.utils.before_app_install"
-# after_app_install = "ferum_customs.utils.after_app_install"
-
-# Integration Cleanup
-# -------------------
-# To clean up dependencies/integrations with other apps
-# Name of the app being uninstalled is passed as an argument
-
-# before_app_uninstall = "ferum_customs.utils.before_app_uninstall"
-# after_app_uninstall = "ferum_customs.utils.after_app_uninstall"
-
-# Desk Notifications
-# ------------------
-# See frappe.core.notifications.get_notification_config
-
-# notification_config = "ferum_customs.notifications.get_notification_config"
-
-# Permissions
-# -----------
-# Permissions evaluated in scripted ways
-
-# permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
-#
-# has_permission = {
-# 	"Event": "frappe.desk.doctype.event.event.has_permission",
-# }
-
-# Document Events
-# ---------------
-# Hook on document methods and events
-
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
-
-# Scheduled Tasks
-# ---------------
-
-# scheduler_events = {
-# 	"all": [
-# 		"ferum_customs.tasks.all"
-# 	],
-# 	"daily": [
-# 		"ferum_customs.tasks.daily"
-# 	],
-# 	"hourly": [
-# 		"ferum_customs.tasks.hourly"
-# 	],
-# 	"weekly": [
-# 		"ferum_customs.tasks.weekly"
-# 	],
-# 	"monthly": [
-# 		"ferum_customs.tasks.monthly"
-# 	],
-# }
-
-# Testing
-# -------
-
-# before_tests = "ferum_customs.install.before_tests"
-
-# Overriding Methods
-# ------------------------------
-#
-# override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "ferum_customs.event.get_events"
-# }
-#
-# each overriding function accepts a `data` argument;
-# generated from the base implementation of the doctype dashboard,
-# along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "ferum_customs.task.get_dashboard_data"
-# }
-
-# exempt linked doctypes from being automatically cancelled
-#
-# auto_cancel_exempted_doctypes = ["Auto Repeat"]
-
-# Ignore links to specified DocTypes when deleting documents
-# -----------------------------------------------------------
-
-# ignore_links_on_delete = ["Communication", "ToDo"]
-
-# Request Events
-# ----------------
-# before_request = ["ferum_customs.utils.before_request"]
-# after_request = ["ferum_customs.utils.after_request"]
-
-# Job Events
-# ----------
-# before_job = ["ferum_customs.utils.before_job"]
-# after_job = ["ferum_customs.utils.after_job"]
-
-# User Data Protection
-# --------------------
-
-# user_data_fields = [
-# 	{
-# 		"doctype": "{doctype_1}",
-# 		"filter_by": "{filter_by}",
-# 		"redact_fields": ["{field_1}", "{field_2}"],
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_2}",
-# 		"filter_by": "{filter_by}",
-# 		"partial": 1,
-# 	},
-# 	{
-# 		"doctype": "{doctype_3}",
-# 		"strict": False,
-# 	},
-# 	{
-# 		"doctype": "{doctype_4}"
-# 	}
-# ]
-
-# Authentication and authorization
-# --------------------------------
-
-# auth_hooks = [
-# 	"ferum_customs.auth.validate"
-# ]
-
-# Automatically update python controller files with type annotations for this app.
-# export_python_type_annotations = True
-
-# default_log_clearing_doctypes = {
-# 	"Logging DocType Name": 30  # days to retain logs
-# }
-
+try:  # dev-hooks (если есть)
+	from .dev_hooks import *
+except ImportError:
+	pass
